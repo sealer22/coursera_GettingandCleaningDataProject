df33e325f418651a00c5b6e85fd8a945d7319376
sealer <sealer22@gmail.com>
2014-10-20 22:55:31 +0800

    1st

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 coursera_GettingandCleaningDataProject
 ======================================
-1. Intro:
+##1. Intro:
+
 This repository is for coursera:Getting andCleaning Data project2.
 
-2. Dataset:
+##2. Dataset:
+
 You can download the dataset from "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-3. Script:
+##3. Script:
+
 There is a R script called "run_analysis.R". 
 
 Please make sure you 
-  -extract the UCI HAR Dataset within the workspace before you run the script.
-  -install the data.table and reshape2 library 
+  
+*extract the UCI HAR Dataset within the workspace before you run the script.
+*install the data.table and reshape2 library 
   
 The script will 
-  -use the UCI HAR Dataset as its input 
-  -combine the train and test dataset 
-  -specify the measurments of mean and standard deviation 
-  -calculate the means of all the columns based on subject and activity. 
-  -write the result into a file called tiny_data.txt(sep by ",")
+
+ *use the UCI HAR Dataset as its input 
+*combine the train and test dataset 
+*specify the measurments of mean and standard deviation 
+*calculate the means of all the columns based on subject and activity. 
+*write the result into a file called tiny_data.txt(sep by ",")
---
+++ run_analysis.R
@@ -0,0 +1,67 @@
+library("data.table")
+library("reshape2")
+##load library
+
+
+test_x <- read.table("./UCI HAR Dataset/test/X_test.txt",header=F)
+test_y <- read.table("./UCI HAR Dataset/test/y_test.txt",header=F)
+test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=F)
+train_x <- read.table("./UCI HAR Dataset/train/X_train.txt",header=F)
+train_y <- read.table("./UCI HAR Dataset/train/y_train.txt",header=F)
+train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=F)
+activity_label <- read.table("./UCI HAR Dataset/activity_labels.txt",header=F)
+feature <- read.table("./UCI HAR Dataset/features.txt",header=F)
+##read the train data, test data, label, and feature
+
+
+feature_mean_std <- grepl("mean|std", feature[,2])
+##find all the mean and standard deviation within feature
+
+
+names(test_x) = feature[,2]
+names(train_x) = feature[,2]
+
+
+test_x = test_x[,feature_mean_std]
+train_x  = train_x[,feature_mean_std]
+##find all measurments of mean and standard deviation
+
+
+test_y[,2] = activity_label[test_y[,1],2]
+train_y[,2] = activity_label[train_y[,1],2]
+##load labels
+
+
+names(test_y) = c("Activity", "Activity_Label")
+names(test_subject) = "subject"
+names(train_y) = c("Activity", "Activity_Label")
+names(train_subject) = "subject"
+##give names to test_y,train_y, test_subject, and train_subject
+
+
+test_all <- cbind(test_x,test_y)
+test_all <- cbind(test_all,test_subject)
+##combine the test data
+
+
+train_all <- cbind(train_x,train_y)
+train_all <- cbind(train_all,train_subject)
+##combine the train data
+
+
+data_all <- rbind(test_all,train_all)
+##combine the train data and the test data
+
+
+ids = c("Activity", "Activity_Label", "subject")
+data_labels = setdiff(colnames(data_all), ids)
+data_melt = melt(data_all, id = ids, measure.vars = data_labels)
+##melt the data
+
+
+data_tidy <- dcast(data_melt , Activity + subject ~ variable, mean) 
+# recast the data
+
+
+write.table(data_tidy, file = "./tidy_data.txt", sep = ",")
+# write data into file
